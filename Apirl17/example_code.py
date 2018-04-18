c437f87054fade44e34f2184d18bde3f5dfe11fb
dl8sd11 <tmd910607@gmail.com>
2018-04-18 07:03:06 -0700

    guess function

--- Apirl17/example_code.py
+++ Apirl17/example_code.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+def guess():
+    user_input = Entry1.get()
+    if not(user_input.isdigit()):
+        print("Error")
 root =tk.Tk()
 label1 = tk.Label(root,text="輸入數字")
 label1.grid(row=0,column=0)
@@ -7,5 +11,5 @@ Entry1.grid(row=0,column=1)
 button1 = tk.Button(root,text="猜",command=guess)
 button1.grid(row=0,column=2)
 result_lb = tk.Label(root,text="")
-result_lb(row=1,column=0)
+result_lb.grid(row=1,column=0)
 root.mainloop()
